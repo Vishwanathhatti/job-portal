3fbbb71ce417cbad157d78682726b5b907fd0c15
Vishwanathhatti <92014314+Vishwanathhatti@users.noreply.github.com>
2024-08-21 17:58:43 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT= "http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+export const USER_API_END_POINT= "https://job-portal-backend-1zz98g0rj-vishwanaths-projects-ac0072e2.vercel.app/api/v1/user";
+export const JOB_API_END_POINT = "https://job-portal-backend-1zz98g0rj-vishwanaths-projects-ac0072e2.vercel.app/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://job-portal-backend-1zz98g0rj-vishwanaths-projects-ac0072e2.vercel.app/api/v1/application";
+export const COMPANY_API_END_POINT = "https://job-portal-backend-1zz98g0rj-vishwanaths-projects-ac0072e2.vercel.app/api/v1/company";
